ef72818ca762a3388924e2c1d65503ef6f25e259
Jesse Plamondon-Willard <plamondon@brainbankinc.com>
2013-07-04 11:39:35 -0400

    +global assembly info.

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pathoschild.PredicateSecurity")]
+[assembly: AssemblyCopyright("Copyright © Jesse Plamondon-Willard 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.3.0.0")]
